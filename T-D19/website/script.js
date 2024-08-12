e24ac453f9543ed564f1f9f685cf016676d39953
bha1212002 <bhavikchhabria@gmaial.com>
2024-08-12 14:26:05 +0530

    files added success updated

---
+++ T-D19/website/script.js
@@ -0,0 +1,5 @@
+// ~/Desktop/OneDrive/Training_dev/Training_dev/T-D-19/website/script.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript is working!');
+});
+
